7468e1a2f03a71338a7babc109cc09f5d18b8f04
marioalajarin <118173607+marioalajarin@users.noreply.github.com>
2022-11-14 12:46:22 +0100

    Add files via upload

---
+++ actividad15.py
@@ -0,0 +1,31 @@
+import random
+import pandas as pd
+import matplotlib.pyplot as plt
+
+num=random.sample(range(1, 100), 10)
+num.sort()
+print(num)
+
+#--------------------------------------------------------------------
+
+frase=input("Ingresa una frase: ")
+letra=input("Elige la letra que quieres comprobar: ")
+
+print(f"la frase tiene {frase.count(letra)}.")
+
+#---------------------------------------------------------------------
+
+def archivo_csv():
+    datos=pd.read_csv("C:\\Users\\Tecnicos\\Desktop\\Mario-FP\\Programación\Proyectos\\teoria_14112022\\nuevo.csv")
+    df = datos[["QuotaAmount", "StartDate", "OwnerName", "Username"]]
+
+    df = datos.rename(columns={
+        "QuotaAmount": "cantidad",
+        "StartDate": "fecha",
+        "OwnerName": "nombreP",
+        "Username": "nombre"
+    })
+
+    df.nombreP.value_counts().plot.pie()
+    plt.show()
+archivo_csv()
